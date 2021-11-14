ea60720b04c7f10655b4a46c353ab3515d0080b5
Mateusz Zięba <matzieba7@gmail.com>
2021-11-14 21:49:15 +0100

    zadanie lab3

---
+++ Zadanie/Models/FormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zadanie.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "imię")]
+        [Required]
+        public string imie { get; set; }
+        [Display(Name = "ilość_powtórzeń")]
+        public int ilosc_powtorzen { get; set; }
+        public string Belongs { get; set; }
+    }
+}
